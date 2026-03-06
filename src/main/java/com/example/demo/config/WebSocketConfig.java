e036b7bb158f11982050b98fa480d6e540f98ac9
jdh <wjdehd555@gmail.com>
2026-03-06 14:28:12 +0900

    [Feat] 기본 웹 소켓에서 사용자 정보 받기

--- src/main/java/com/example/demo/config/WebSocketConfig.java
+++ src/main/java/com/example/demo/config/WebSocketConfig.java
@@ -1,5 +1,6 @@
 package com.example.demo.config;
 
+import com.example.demo.config.interceptor.JwtHandshakeInterceptor;
 import com.example.demo.config.websocket.WebSocketHandler;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
@@ -12,8 +13,13 @@ import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry
 @RequiredArgsConstructor
 public class WebSocketConfig implements WebSocketConfigurer {
     private final WebSocketHandler webSocketHandler;
+    private final JwtHandshakeInterceptor jwtHandshakeInterceptor;
+
     @Override
     public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
-        registry.addHandler(webSocketHandler, "/ws").setAllowedOrigins("*"); // 웹 소켓에 보안이 따로 있기 때문에 Spring security에서 허용해주는 코드는 다 해놨고 이걸 설정해주기
+        registry.addHandler(webSocketHandler, "/ws")
+                // 웹 소켓에 보안이 따로 있기 때문에 Spring security에서 허용해주는 코드는 다 해놔도 이걸 설정해줘야 한다.
+                .setAllowedOrigins("*")
+                .addInterceptors(jwtHandshakeInterceptor);
     }
 }
