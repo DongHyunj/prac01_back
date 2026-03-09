9f1fdc068031999b877ea0c5b7a8dc915042c529
jdh <wjdehd555@gmail.com>
2026-03-09 16:49:04 +0900

    [Feat] 모니터링 환경 설정
    BulkInsert.java 파일을 통해 더미데이터 생성 후 Jmeter로 부하를 주고 Prometheus와 Grafana를 통해 부하를 모니터링하는 환경을 구축

--- src/main/java/com/example/demo/aop/SimpleAop.java
+++ src/main/java/com/example/demo/aop/SimpleAop.java
@@ -46,19 +46,19 @@ public class SimpleAop {
     }
 
 //    @Before("execution(* com.example.demo.board..*.*(..) )") // PointCut을 안 만들어두면 이렇게 설정해야 한다.
-    @Before("cut()") // 포인트 컷에서 지정한 위치의 클래스의 메서드가 실행되기 전에 현재 메서드 실행
-    public void before(JoinPoint joinPoint) { // JoinPoint joinPoint : 위치나 시점, 특정 클래스, 특정 메서드라는 정보
-        Method method = ((MethodSignature)joinPoint.getSignature()).getMethod();
-
-        System.out.println(joinPoint.getSignature());
-        System.out.println(method.getName() + " 메서드 실행 전");
-    }
-
-    @After("cut()") // 포인트 컷에서 지정한 위치의 클래스의 메서드가 실행된 후에 현재 메서드 실행
-    public void after(JoinPoint joinPoint) {
-        Method method = ((MethodSignature)joinPoint.getSignature()).getMethod();
-
-        System.out.println(joinPoint.getSignature());
-        System.out.println(method.getName() + " 메서드 실행 후");
-    }
+//    @Before("cut()") // 포인트 컷에서 지정한 위치의 클래스의 메서드가 실행되기 전에 현재 메서드 실행
+//    public void before(JoinPoint joinPoint) { // JoinPoint joinPoint : 위치나 시점, 특정 클래스, 특정 메서드라는 정보
+//        Method method = ((MethodSignature)joinPoint.getSignature()).getMethod();
+//
+//        System.out.println(joinPoint.getSignature());
+//        System.out.println(method.getName() + " 메서드 실행 전");
+//    }
+//
+//    @After("cut()") // 포인트 컷에서 지정한 위치의 클래스의 메서드가 실행된 후에 현재 메서드 실행
+//    public void after(JoinPoint joinPoint) {
+//        Method method = ((MethodSignature)joinPoint.getSignature()).getMethod();
+//
+//        System.out.println(joinPoint.getSignature());
+//        System.out.println(method.getName() + " 메서드 실행 후");
+//    }
 }
